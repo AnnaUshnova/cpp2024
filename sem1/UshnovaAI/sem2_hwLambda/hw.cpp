89aec3f84aaad54dfa4cacc0c387f98184b1fe45
anna <anna.ushnova@gmail.com>
2025-05-20 19:28:12 +0300

    hw lambda expressions

---
+++ sem1/UshnovaAI/sem2_hwLambda/hw.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <map>
+
+int main() {
+    std::vector<std::string> strings = { "lambda", "expression" };
+
+    auto getLengthsMap = [](const std::vector<std::string>& vec) {
+        std::map<std::string, size_t> strLengthMap;
+        for (const auto& s : vec) {
+            strLengthMap[s] = s.size();
+        }
+        return strLengthMap;
+    };
+
+    auto lengthMap = getLengthsMap(strings);
+    for (const auto& [str, len] : lengthMap) {
+        std::cout << str << ": " << len << std::endl;
+    }
+
+    return 0;
+}
